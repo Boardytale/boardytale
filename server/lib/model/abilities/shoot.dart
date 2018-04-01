dbeb000d4ec4966c5e52754d63d06811ff62a480
Petr Hošek <ringael@seznam.cz>
2018-04-01 22:39:50 +0200

    unit shooting working

--- server/lib/model/abilities/shoot.dart
+++ server/lib/model/abilities/shoot.dart
@@ -1,10 +1,13 @@
 part of boardytale.server.abilities;
 
 class ShootAbility extends commonLib.ShootAbility implements ServerAbility {
-
   @override
-  String perform(Unit invoker,commonLib.Track track) {
-    // TODO: implement perform
-    return "";
+  String perform(Unit invoker, commonLib.Track track) {
+    commonLib.Alea alea = new commonLib.Alea(invoker.attack);
+    int damage = 0;
+    track.last.units.forEach((commonLib.Unit unit) {
+      damage += unit.harm(alea);
+    });
+    return damage.toString();
   }
 }
